3237deb957c53ec85478f74c25d14cb970ea5069
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2023-01-07 15:19:28 +0530

    partial implementation of write & exit syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,18 +3,76 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
 
-static void syscall_handler (struct intr_frame *);
+#define CONSOLE_OUTPUT 1
 
-void
-syscall_init (void) 
+static void syscall_handler(struct intr_frame *);
+static void syscall_exit(int status);
+static int syscall_write(int fd, const void *buffer, unsigned size);
+
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  // TODO check valid esp
+
+  int syscall_type = *(int *)f->esp;
+  // printf("syscall %d \n", syscall_type);
+
+  switch (syscall_type)
+  {
+  case SYS_EXIT:
+  {
+    int status = *(((int *)f->esp) + 1);
+    syscall_exit(status);
+    break;
+  }
+
+  case SYS_WRITE:
+  {
+    // get args
+    int fd = *((int *)f->esp + 1);
+    void *buffer = (void *)(*((int *)f->esp + 2));
+    unsigned size = *((unsigned *)f->esp + 3);
+
+    f->eax = syscall_write(fd, buffer, size);
+    break;
+  }
+
+  default:
+  {
+    break;
+  }
+  }
+}
+
+static void syscall_exit(int status)
+{
+  struct thread *t = thread_current();
+  printf("%s: exit(%d)\n", t->name, status);
+  thread_exit();
+}
+
+static int syscall_write(int fd, const void *buffer, unsigned size)
+{
+  char *_buffer = (char *)buffer;
+  int written_size = 0;
+
+  if (fd == CONSOLE_OUTPUT)
+  {
+    putbuf(_buffer, size);
+    written_size = size;
+  }
+  else
+  {
+    // TODO write to files
+  }
+
+  return written_size;
 }
